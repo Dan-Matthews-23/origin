772e37b994fc7a0bee207959e5c608c30b046ad3
Dan Matthews <dan.matthews1987@gmail.com>
2024-06-18 15:24:43 +0000

    UPDATED overview, nav bar to include active players count

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -7,4 +9,14 @@ def index(request):
     return render(request, 'home/index.html')
 
 def overview(request):
-    return render(request, 'home/overview.html')
+    threshold = timezone.now() - timezone.timedelta(minutes=30)
+    active_user_count = User.objects.filter(last_login__gte=threshold).count()
+    context = {'active_user_count': active_user_count}
+    return render(request, 'home/overview.html', context)
+
+
+
+
+
+
+
